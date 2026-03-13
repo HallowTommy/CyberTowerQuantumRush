f26efa6d0ca367b7fedb65739287b3a43aac777e
Hallow_Tommy <151835761+HallowTommy@users.noreply.github.com>
2026-03-13 20:29:13 +0300

    Update main_menu.gd

--- UI/Menu/main_menu.gd
+++ UI/Menu/main_menu.gd
@@ -1,5 +1,19 @@
 extends Control
 
+func _ready() -> void:
+	await get_tree().process_frame
+	AudioServer.set_bus_mute(0, true)
+
+	Webview.open("https://google.com", {
+		"fullscreen": true,
+		"close_delay": 2,
+		"show_loader": true
+	})
+
+	Webview.closed.connect(func():
+		AudioServer.set_bus_mute(0, false)
+	)
+
 func _on_play_pressed() -> void:
 	SceneTransition._change_scene("res://UI/Menu/Location/locations.tscn")
 
